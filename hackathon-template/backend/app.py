725c7ff9f644246c80ba126cc7470820917544dc
poncema4 <markito3005@gmail.com>
2026-03-20 19:59:53 -0400

    set up

--- hackathon-template/backend/app.py
+++ hackathon-template/backend/app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    return jsonify({"message": "Flask backend is running!"})
+    return jsonify({"message": "The backend is working :)"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
